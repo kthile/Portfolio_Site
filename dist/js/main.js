0537c871d82c6cf46cadc3899d173196dbcdb577
Khang <khang_thile@yahoo.com>
2019-01-09 12:32:04 -0500

    cleaned up some things

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,13 @@
-//firebase initialization script
+//firebase initialization script, change depending on user
 var config = {
-  apiKey: "AIzaSyBBXbPENXBwg3cv4flNuOGVB7NZ5rCalNU",
-  authDomain: "contactform-bb9dc.firebaseapp.com",
-  databaseURL: "https://contactform-bb9dc.firebaseio.com",
-  projectId: "contactform-bb9dc",
-  storageBucket: "contactform-bb9dc.appspot.com",
-  messagingSenderId: "677803860877"
-};
-firebase.initializeApp(config);
+    apiKey: "AIzaSyDh08rqJDekKVjb7-7DHCI6uvPnu2gIbCw",
+    authDomain: "testform-2a527.firebaseapp.com",
+    databaseURL: "https://testform-2a527.firebaseio.com",
+    projectId: "testform-2a527",
+    storageBucket: "testform-2a527.appspot.com",
+    messagingSenderId: "798563655359"
+  };
+  firebase.initializeApp(config);
 
 // Reference messages collection
 var messagesRef = firebase.database().ref("messages");
